ad81e7de2a44df94bdda80cd90825c93d7aaac54
GertScheers <gert378@gmail.com>
2026-06-13 12:04:08 +0200

    add setting to keep screen on

--- courtscorewear/src/main/java/com/gitje/courtscorewear/AppModule.kt
+++ courtscorewear/src/main/java/com/gitje/courtscorewear/AppModule.kt
@@ -1,7 +1,6 @@
 package com.gitje.courtscorewear
 
 import com.gitje.courtscorewear.logic.BadmintonViewModel
-import com.gitje.courtscorewear.logic.BaseViewModel
 import com.gitje.courtscorewear.logic.TennisPadelViewModel
 import org.koin.dsl.module
 
